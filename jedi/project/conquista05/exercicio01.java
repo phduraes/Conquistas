51f616e97c1e3f28136f9f36df11ba8d82357a33
phduraes <phduraes@gmail.comgit config --global user.email phduraes@gmail.com>
2022-06-20 16:45:58 -0300

    Full time

--- jedi/project/conquista05/exercicio01.java
+++ jedi/project/conquista05/exercicio01.java
@@ -1,5 +1,7 @@
 package jedi.project.conquista05;
 
+import java.io.PrintStream;
+
 /**
 Criar um array do tipo String de 10 posições e inicializar.
 Para cada iteração do for, o valor da posição deve ser
@@ -9,5 +11,22 @@ Nome nome
 Nome nome casa
 
  */
-public class exercicio01 {
+
+    public class exercicio01 {
+
+
+    public static void main(String[] args) {
+
+        String [] nomes = new String[4];
+
+        nomes[0] = "Panda \n";
+        nomes[1] = "Gordo \n";
+        nomes[2] = "Kamy \n";
+        nomes[3] = "Neve \n";
+
+        for (int i = 0; i <nomes.length; i++) {
+            PrintStream printf = System.out.printf(nomes[i]);
+        }
+ }
+
 }
