a353f76febdac8df52f6c4d971bd7cf2307e93e4
phduraes <phduraes@gmail.comgit config --global user.email phduraes@gmail.com>
2022-07-04 15:30:40 -0300

    Full time

--- maranota_java/Aula02TiposPrimitivos.java
+++ maranota_java/Aula02TiposPrimitivos.java
@@ -1,23 +1,33 @@
 package maranota_java;
 
+import java.io.PrintStream;
+
 public class Aula02TiposPrimitivos {
 
     //int, double, float, char, byte, short, long., boolena
 
     public static void main(String[] args) {
-        int idade = 46;
-        long numeroGrande = 1000000;
-        double salarioDouble = 20000;
-        float salarioFloat = 2500;
-        byte idadeByte = -128;
+        int idade = (int) 1000000000L;
+        long numeroGrande = 1000000L;
+        double salarioDouble = 2000.0;
+        float salarioFloat = (float) 2500.0D;
+        byte idadeByte = 127;
+        short idadeShort = 32000;
         boolean verdadeiro = true;
-        boolean falso= false;
-        char caractere = 10;
+        boolean falso = false;
+        char caractere = '\u0041';
 
-        System.out.printf(String.valueOf("a idade e de " + idade + " anos"));
+        String nome = " Goku";
+        var nome2 = "Vegetal" +
+                       System.out.printf(String.valueOf(" a idade e de " + idade + " anos"));
+        System.out.printf(String.valueOf(verdadeiro));
+        System.out.printf(String.valueOf(falso));
+        System.out.printf("char " + caractere);
+        System.out.printf(String.valueOf(idade));
+        System.out.printf(String.valueOf(salarioFloat));
+        System.out.printf("\n oi meu nome e " +nome);
 
-
-           }
+    }
 
 
 }
