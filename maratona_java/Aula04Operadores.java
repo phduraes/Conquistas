765590edef39c810cf4b371d29cba4ff8044f70f
phduraes <phduraes@gmail.comgit config --global user.email phduraes@gmail.com>
2022-07-04 16:14:00 -0300

    Full time

---
+++ maratona_java/Aula04Operadores.java
@@ -0,0 +1,18 @@
+package maratona_java;
+
+public class Aula04Operadores {
+
+    public static void main(String[] args) {
+
+        // + - * /
+
+        int numero01 = 10;
+        int numero02 = 20;
+      //  int resultado = numero01 * numero02;
+        double resultado2 = numero01 / numero02;
+
+      //  System.out.printf(String.valueOf(resultado));
+        System.out.printf(String.valueOf(resultado2));
+
+    }
+}
