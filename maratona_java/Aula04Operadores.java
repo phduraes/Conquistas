434160935af06592ba6ae784d82a5bc9f7d344ad
phduraes <phduraes@gmail.comgit config --global user.email phduraes@gmail.com>
2022-07-05 14:04:45 -0300

    Full time

--- maratona_java/Aula04Operadores.java
+++ maratona_java/Aula04Operadores.java
@@ -1,5 +1,7 @@
 package maratona_java;
 
+import javax.sound.midi.Soundbank;
+
 public class Aula04Operadores {
 
     public static void main(String[] args) {
@@ -26,21 +28,50 @@ public class Aula04Operadores {
 
 
 
-        System.out.printf(" isDezMaiorQueVinte " +isDezMaiorQueVinte);
-        System.out.printf(" isDezIgualVinte " +isDezIgualVinte);
-        System.out.printf(" isDezIgualDez" +isDezIgualDez);
-        System.out.printf(" isDezdiferenteDez" +isDezIgualDez);
+        System.out.printf("\n isDezMaiorQueVinte " +isDezMaiorQueVinte);
+        System.out.printf("\n isDezIgualVinte " +isDezIgualVinte);
+        System.out.printf("\n isDezIgualDez" +isDezIgualDez);
+        System.out.printf("\n isDezdiferenteDez" +isDezIgualDez);
+
+
+        //  && (and) || (or) !
+
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 & salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade <= 30 & salario >= 3381;
+        System.out.printf("\n isDentroDaLeiMaiorQueTrinta " +isDentroDaLeiMaiorQueTrinta);
+        System.out.printf("\n isDentroDaLeiMenorQueTrinta" +isDentroDaLeiMenorQueTrinta);
 
 
 
+        double valorTotalContaCorrente = 5000;
+        double valorTotalContaPoupanca = 100000;
+        float valorPlaystation = 4500F;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorPlaystation > valorPlaystation;
+        System.out.printf(" \n isPlaystationCincoCompravel " +isPlaystationCincoCompravel);
 
 
 
+        // = += -= /= %=
+        double bonus = 1800;
+        bonus += 1000; // 2800
+        bonus -= 1000; // 1800
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+        System.out.printf(String.valueOf(bonus));
 
 
-
-
-
+        // ++ --
+        int contador = 0;
+        contador += 1; // contador = contador + 1
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        //int contador2 = 0;
+        System.out.printf(String.valueOf(contador));
 
 
     }
