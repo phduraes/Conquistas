0d4d0efb5edc4ca122d7887a28de09035897f586
phduraes <phduraes@gmail.comgit config --global user.email phduraes@gmail.com>
2022-07-05 08:36:17 -0300

    Full time

--- maratona_java/Aula04Operadores.java
+++ maratona_java/Aula04Operadores.java
@@ -8,11 +8,40 @@ public class Aula04Operadores {
 
         int numero01 = 10;
         int numero02 = 20;
-      //  int resultado = numero01 * numero02;
-        double resultado2 = numero01 / numero02;
+        int resultado = (int) (numero01 / (double) numero02);
 
-      //  System.out.printf(String.valueOf(resultado));
-        System.out.printf(String.valueOf(resultado2));
+       System.out.printf(String.valueOf(resultado));
+
+       // %
+        int resto = 21 % 7;
+        System.out.printf(String.valueOf(resto));
+
+        // < > <= >= == !=
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezdiferenteDez = 10 != 10;
+
+
+
+        System.out.printf(" isDezMaiorQueVinte " +isDezMaiorQueVinte);
+        System.out.printf(" isDezIgualVinte " +isDezIgualVinte);
+        System.out.printf(" isDezIgualDez" +isDezIgualDez);
+        System.out.printf(" isDezdiferenteDez" +isDezIgualDez);
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
