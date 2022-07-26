910794eab4731624433dc469623401e42040025a
phduraes <phduraes@gmail.comgit config --global user.email phduraes@gmail.com>
2022-07-26 14:38:14 -0300

    Full time

--- javacore/introducaometodos/dominio/Calculadora.java
+++ javacore/introducaometodos/dominio/Calculadora.java
@@ -1,6 +1,15 @@
 package javacore.introducaometodos.dominio;
 
+/**
+ *  44 - Orientação Objetos - Métodos pt 01
+ */
+
 public class Calculadora {
 
+    public void somaDoisNumeros () {
+
+        System.out.printf(String.valueOf(10 + 10));
+
+    }
 
 }
---
+++ javacore/introducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,14 @@
+package javacore.introducaometodos.test;
+
+import javacore.introducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+
+    public static void main(String[] args) {
+
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        System.out.printf("\nFinalizano Calculadora Test01 ");
+
+    }
+}
